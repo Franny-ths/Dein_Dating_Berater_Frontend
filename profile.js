9005d6ed42db08ef7c36e6b2a18a7bbd2bd153a5
Franny-ths <wolfsrudelchen@gmail.com>
2024-12-18 17:16:55 +0100

    Bewertungssystem_verbessert

--- profile.js
+++ profile.js
@@ -1,18 +1,43 @@
 const runLocal = false;
+const debugLog = false;
 let url = "";
 
 if (runLocal) {
     url = "http://localhost:5000/api/";
+    console.log(`Backend server running on http://localhost:5000`);
+    console.log(`Profile Database on http://localhost:5000/api/admin/profiles`)
+    console.log(`Reset Profile Database on http://localhost:5000/api/admin/reset-profiles`)
 } else {
     url = "https://dein-dating-berater-backend.onrender.com/api/";
+    console.log(`Backend server running on https://dein-dating-berater-backend.onrender.com`);
+    console.log(`Profile Database on https://dein-dating-berater-backend.onrender.com/api/admin/profiles`);
+    console.log(`Reset Profile Database on https://dein-dating-berater-backend.onrender.com/api/admin/reset-profiles`)
 }
 
+;
 
 const waitOverlay = document.getElementById('wait-overlay');
 const waitMessage = document.getElementById('wait-message');
 
-// Function to update the profile dynamically
+let selectedRating = null;
+let compareableProfiles = {
+    originalProfile: null,
+    improvedProfile: null,
+    improvement_message: null,
+};
+const rating_div = document.getElementById("rating_field");
+const thumbUp = document.getElementById("thumb-up");
+const thumbDown = document.getElementById("thumb-down");
+const ratingButton = document.getElementById("submit-rating");
+
+thumbUp.addEventListener("click", notYetReady);
+thumbDown.addEventListener("click", notYetReady);
+document.getElementById("submit-rating").addEventListener("click", notYetReady);
+
+// Methode um die Daten anzuzeigen
 function updateProfile(data) {
+    document.getElementById('headline_output').textContent = "Verbessertes Profil:";
+
     document.getElementById('name').textContent = data.name;
     document.getElementById('age').textContent = data.age + ' Jahre';
     document.getElementById('height').textContent = data.height + ' cm';
@@ -24,9 +49,9 @@ function updateProfile(data) {
     document.getElementById('job').textContent = "Beruf: " + data.job;
     document.getElementById('interests').textContent = "Interessen: " + data.interests;
     document.getElementById('searchFor').textContent = "Auf der Suche nach: " + data.searchFor;
-
 }
 
+// Beim drücken auf dem Button werden die eingetragenen Daten gesammelt und weitergeleitet
 document.getElementById('submit-btn').addEventListener('click', function () {
     const profileData = {
         name: document.getElementById('name-input').value,
@@ -42,93 +67,88 @@ document.getElementById('submit-btn').addEventListener('click', function () {
         searchFor: document.getElementById('searchFor-input').value
     };
 
-    console.log(profileData);
+    if (debugLog) console.log(profileData);
     sendProfileData(profileData);
-
-    const imageInput = document.getElementById('profile-image-input');
-    if (imageInput.files.length > 0) {
-        const imageFile = imageInput.files[0];
-        displayImagePreview(imageFile);  // Display the image without sending it to the backend
-    }
-
-
-
-
 
 });
 
-let compareableProfiles = {
-    originalProfile: null,
-    improvedProfile: null,
-};
 
-// Function to display the uploaded image on the right side
+
+// Methode um das Bild einzulesen und wieder anzuzeigen
 function displayImagePreview(imageFile) {
     const reader = new FileReader();
     reader.onload = function (e) {
         const profilePicture = document.getElementById('profile-picture');
         profilePicture.src = e.target.result;
-        profilePicture.style.display = 'block'; // Show the image
+        profilePicture.style.display = 'block'; 
     };
-    reader.readAsDataURL(imageFile); // Convert the image to Base64 and show preview
+    reader.readAsDataURL(imageFile); 
 }
 
+// Methode um Profildaten an das Backend zu senden
 async function sendProfileData(profileData) {
 
+    // Wartenachricht anzeigen
     waitMessage.innerHTML = "Bitte warte während dein Profil verbessert wird...";
     waitOverlay.style.display = 'flex';
-    compareableProfiles.originalProfile = JSON.stringify(profileData);  // Store original profile as a string
+
+    // Original Profil speichern
+    compareableProfiles.originalProfile = JSON.stringify(profileData);  
 
     try {
-        const response = await fetch(url+'improve-profile', {
+        const response = await fetch(url + 'improve-profile', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ profileData })  // Send the profile data in the request body
+            body: JSON.stringify({ profileData })  // Profildaten an Backend senden und verbesserte Version empfangen
         });
 
 
-        const result = await response.json();  // Parse the JSON response
-        console.log("Result from Backend: " + result.improvedProfile);
+        const result = await response.json();  
+        if (debugLog) console.log("Antwort vom Backend: " + result.improvedProfile);
         const data = result.improvedProfile
 
-        compareableProfiles.improvedProfile = data; // store the improved profile
+        compareableProfiles.improvedProfile = data; // Verbesserte Version speichern
+        if (debugLog) console.log("Zu vergleichende Profile: ");
+        if (debugLog) console.log("Originales Profil: " + compareableProfiles.originalProfile + " &  Verbessertes Profile: " + compareableProfiles.improvedProfile);
 
+        // Daten im Fenster anzeigen
         updateProfile(JSON.parse(data));
-        console.log("Compareable Profiles: ");
-        console.log("Original Profile: " +compareableProfiles.originalProfile + " &  Improved Profile: "+ compareableProfiles.improvedProfile);
-        thumbUp.removeEventListener("click",notYetReady);
-        thumbDown.removeEventListener("click",notYetReady);
-        ratingButton.removeEventListener("click",notYetReady);
+
+        // Bild Einfügen
+        const imageInput = document.getElementById('profile-image-input');
+        if (imageInput.files.length > 0) {
+            const imageFile = imageInput.files[0];
+            displayImagePreview(imageFile);
+        }
+
+        // Bewertungssystem vorbereiten
+        thumbUp.removeEventListener("click", notYetReady);
+        thumbDown.removeEventListener("click", notYetReady);
+        ratingButton.removeEventListener("click", notYetReady);
         thumbUp.addEventListener("click", workingUp);
         thumbDown.addEventListener("click", workingDown);
         ratingButton.addEventListener("click", submitRating);
 
+        // Bewertungsfeld anzeigen
+        rating_div.style.display = "block";
+
     } catch (error) {
-        console.error('Error while communicating with the backend:', error);
-        alert('Error while communicating with the backend:', error);
+        console.error('Error beim kommunizieren mit dem Backend:', error);
+        alert('Error beim kommunizieren mit dem Backend:', error);
     } finally {
         waitOverlay.style.display = 'none';
-
     }
 }
 
-let selectedRating = null;
-const thumbUp = document.getElementById("thumb-up");
-const thumbDown = document.getElementById("thumb-down");
-const ratingButton = document.getElementById("submit-rating");
 
-thumbUp.addEventListener("click", notYetReady);
-thumbDown.addEventListener("click", notYetReady);
-document.getElementById("submit-rating").addEventListener("click", notYetReady);
-
+// Methoden 
 function workingUp() {
     selectedRating = 1;
     thumbUp.classList.add("selected");
     thumbDown.classList.remove("down-selected");
     ratingButton.classList.add("rated");
-    console.log(compareableProfiles);
 }
 
 function workingDown() {
@@ -136,7 +156,6 @@ function workingDown() {
     thumbDown.classList.add("down-selected");
     thumbUp.classList.remove("selected");
     ratingButton.classList.add("rated");
-    console.log(compareableProfiles);
 }
 
 
@@ -147,14 +166,20 @@ function notYetReady() {
 // Submit rating to backend
 async function submitRating() {
     if (selectedRating === null) {
-        alert("Bitte wähle eine Verbesserung aus bevor du sie abschickst.");
+        alert("Bitte wähle eine Berwertung aus bevor du sie abschickst.");
         return;
     }
     waitMessage.innerHTML = "Bitte warte während deine Bewertung hochgeladen wird...";
     waitOverlay.style.display = 'flex';
 
+    const improvement_message = document.getElementById('improvement_message_input').value;
+
+    if (improvement_message != null | "") {
+        compareableProfiles.improvement_message = improvement_message;
+    }
+
     try {
-        const response = await fetch(url+"uploadResults", {
+        const response = await fetch(url + "uploadResults", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -162,17 +187,19 @@ async function submitRating() {
             body: JSON.stringify({
                 originalProfile: compareableProfiles.originalProfile,
                 improvedProfile: compareableProfiles.improvedProfile,
-                rating: selectedRating
+                rating: selectedRating,
+                improvement_message: compareableProfiles.improvement_message
             }),
         });
 
         const data = await response.json();
-        console.log("Rating submitted successfully:", data);
+        console.log("Bewertung erfolgreich hochgeladen:", data);
+        rating_div.style.display = "none";
+
     } catch (error) {
-        console.error("Error while submitting rating:", error);
-        alert("Error while submitting rating:", error);
+        console.error("Error beim Hochladen der Bewertung", error);
+        alert("Error beim Hochladen der Bewertung:", error);
     } finally {
         waitOverlay.style.display = 'none';
-
     }
 }
